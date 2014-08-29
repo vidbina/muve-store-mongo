5463730a40be1acbac4a53c05427b95a5e55a0ac
David Asabina <david@supr.nu>
2014-08-29 23:58:37 +0200

    Some basic id tests + conversion to proper id type

--- lib/muve-store-mongo.rb
+++ lib/muve-store-mongo.rb
@@ -18,8 +18,10 @@ module Muve
         # TODO: discover a solution that works for situations where database 
         # driver returns string keys as well as symbol keys
         raise Muve::Error::InvalidAttribute, "Invalid details" unless details.kind_of? Hash
+        raise Muve::Error::InvalidQuery, "The id or details need to be set" if (id.nil? && details.empty?)
+        id = ((BSON::ObjectId.from_string(id) if id.kind_of? String) or id)
         result = resource.database[resource.container].find_one(details.merge(_id: id))
-        raise Muve::Error::InvalidAttribute, "#{resource} #{id} is not found" unless result
+        raise Muve::Error::NotFound, "#{resource} #{id} is not found" unless result
         result = Helper.symbolize_keys(result)
         result[:id] = result.delete(:_id)
         result
@@ -39,6 +41,7 @@ module Muve
       def self.update(resource, id, details)
         raise Muve::Error::InvalidAttribute, "Invalid details" unless details.kind_of? Hash
         # TODO: raise error if details is not valid
+        id = ((BSON::ObjectId.from_string(id) if id.kind_of? String) or id)
         resource.database[resource.container].find_and_modify(
           query: { _id: id },
           update: details
@@ -47,6 +50,7 @@ module Muve
 
       def self.delete(resource, id, details={})
         raise Muve::Error::InvalidAttribute, "Invalid details" unless details.kind_of? Hash
+        id = ((BSON::ObjectId.from_string(id) if id.kind_of? String) or id)
         details = details.merge(_id: id) if id
         resource.database[resource.container].remove(details)
       end
